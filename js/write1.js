fc6ef939f081250a5adf635a12a5a068e8de7fd6
berezina-na <berezina-na-kostroma@yandex.ru>
2017-10-12 06:57:03 +0300

    js

--- js/write1.js
+++ js/write1.js
@@ -1,10 +1,38 @@
 
 
-var formOpen=document.querySelector("btn-adr");
-modal=document.querySelector("write-us");
-close=document.querySelector("close");
-cancel=document.querySelector("cancel");
+var formOpen = document.querySelector(".btn-adr");
+var modal = document.querySelector(".write-us");
+var fio=document.querySelector("[name=name]");
+var mail=document.querySelector("[name=e-mail]");
+var submit=document.querySelector(".send");
+var form=document.querySelector(".form-write-us");
+var close = document.querySelector(".close");
+var error=modal.querySelector("span");
 console.log("Привет");
-formOpen.addEventListener("click",function(e){e.preventDefault(),modal.classList.add("write-us-show")});
-close.addEventListener("click",function(e){e.preventDefault(),modal.classList.remove("write-us-show")});
-cancel.addEventListener("click",function(e){e.preventDefault(),modal.classList.remove("write-us-show")});
+
+formOpen.addEventListener("click",function(e){
+    e.preventDefault();
+    modal.classList.toggle("write-us-show");
+    fio.focus();
+});
+close.addEventListener("click",function(e){
+    e.preventDefault();
+    modal.classList.remove("write-us-show")});
+cancel.addEventListener("click",function(e){
+    e.preventDefault();
+    modal.classList.remove("write-us")});
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(!fio.value || !mail.value) {
+        error.classList.add("error-name");
+    }
+    else {
+
+    }
+    console.log("я не отправилась");
+});
+window.addEventListener("keydown",function(e){
+    if(evt.keyCode === 27){
+modal.classList.remove("write-us");
+    }
+});
